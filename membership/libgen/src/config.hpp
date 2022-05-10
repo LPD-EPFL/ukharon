9ba37b15d4226f4c21e59a3877a2b57d52620da6
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-05-10 15:21:38 +0200

    Adjust the multicast group from 0xc003 -> 0xc004 in libmembership.so

--- membership/libgen/src/config.hpp
+++ membership/libgen/src/config.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-#define MCGROUP "ff12:601b:ffff::1:ff28:cf2a/0xc003"
-#define KERNEL_MCGROUP "ff12:401b:ffff::1/0xc003"
+#define MCGROUP "ff12:601b:ffff::1:ff28:cf2a/0xc004"
+#define KERNEL_MCGROUP "ff12:401b:ffff::1/0xc004"
