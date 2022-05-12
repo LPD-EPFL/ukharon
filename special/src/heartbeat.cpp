aced918aee354e5fb646766fa7c3ea43d82bae6c
Athanasios Xygkis <athanasios.xygkis@epfl.ch>
2022-05-12 08:57:31 +0200

    special: Improve kernel detection code

--- special/src/heartbeat.cpp
+++ special/src/heartbeat.cpp
@@ -19,34 +19,8 @@ namespace dory::special {
 auto logger = dory::std_out_logger("SPECIAL");
 
 void enable_heartbeat(int data) {
-  struct utsname buf;
-
-  // Check the kernel version
-  auto ret_uname = uname(&buf);
-  if (ret_uname == -1) {
-    throw std::runtime_error("Could not read the kernel version (" +
-                             std::to_string(errno) +
-                             "): " + std::string(std::strerror(errno)));
-  }
-
-  bool found = false;
-  for (const auto& current : heartbeat::compatible_kernels) {
-    if (std::string(buf.release) != std::string(current.release)) {
-      continue;
-    }
-
-    if (std::string(buf.machine) != std::string(current.arch)) {
-      continue;
-    }
-
-    std::size_t found_version =
-        std::string(buf.version).find(std::string(current.version));
-    if (found_version == std::string::npos) {
-      continue;
-    }
-
-    found = true;
-  }
+  auto info = heartbeat::kernel_version();
+  auto found = heartbeat::is_kernel_compatible(info, true);
 
   if (!found) {
     LOGGER_WARN(
@@ -64,4 +38,73 @@ void enable_heartbeat(int data) {
         std::to_string(errno) + "): " + std::string(std::strerror(errno)));
   }
 }
+
+namespace heartbeat {
+std::optional<KernelInfo> from_serialized_kernel_info(std::string const& info) {
+  size_t release_size;
+  size_t version_size;
+  size_t arch_size;
+
+  std::stringstream ss(info);
+
+  ss >> release_size;
+  ss.ignore();
+
+  ss >> version_size;
+  ss.ignore();
+
+  ss >> arch_size;
+  ss.ignore();
+
+  std::string release(release_size, '\0');
+  ss.read(&release[0], static_cast<std::streamsize>(release_size));
+
+  std::string version(version_size, '\0');
+  ss.read(&version[0], static_cast<std::streamsize>(version_size));
+
+  std::string arch;
+  ss >> arch;
+
+  return KernelInfo(release, version, arch);
+}
+
+KernelInfo kernel_version() {
+  struct utsname buf;
+
+  // Check the kernel version
+  auto ret_uname = uname(&buf);
+  if (ret_uname == -1) {
+    throw std::runtime_error("Could not read the kernel version (" +
+                             std::to_string(errno) +
+                             "): " + std::string(std::strerror(errno)));
+  }
+
+  return KernelInfo(buf.release, buf.version, buf.machine);
+}
+
+bool is_kernel_compatible(KernelInfo const& info, bool relaxed) {
+  for (const auto& current : compatible_kernels) {
+    if (info.release != current.release) {
+      continue;
+    }
+
+    if (info.arch != current.arch) {
+      continue;
+    }
+
+    if (relaxed) {
+      if (info.version.find(current.version) != std::string::npos) {
+        return true;
+      }
+    } else {
+      if (info.version == current.version) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+}  // namespace heartbeat
 }  // namespace dory::special
--- special/src/heartbeat.hpp
+++ special/src/heartbeat.hpp
@@ -1,24 +1,43 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <utility>
 
 namespace dory::special {
 void enable_heartbeat(int data);
 
 namespace heartbeat {
 struct KernelInfo {
-  KernelInfo(char const *release, char const *version, char const *arch)
-      : release{release}, version{version}, arch{arch} {}
+  KernelInfo(std::string release, std::string version, std::string arch)
+      : release{std::move(release)},
+        version{std::move(version)},
+        arch{std::move(arch)} {}
 
-  char const *release;
-  char const *version;
-  char const *arch;
+  std::string const release;
+  std::string const version;
+  std::string const arch;
+
+  std::string serialize() const {
+    std::ostringstream s;
+    s << release.size() << "," << version.size() << "," << arch.size() << ","
+      << release << version << arch;
+
+    return s.str();
+  }
 };
+
+std::optional<KernelInfo> from_serialized_kernel_info(std::string const &info);
 
 static KernelInfo const compatible_kernels[] = {
     KernelInfo{"5.4.0-74-custom", "#83+rtcore+heartbeat+nohzfull", "x86_64"}};
 
 std::size_t constexpr CompatibleKernelsNum =
     sizeof(compatible_kernels) / sizeof(KernelInfo);
+
+KernelInfo kernel_version();
+bool is_kernel_compatible(KernelInfo const &info, bool relaxed = false);
 }  // namespace heartbeat
 }  // namespace dory::special
